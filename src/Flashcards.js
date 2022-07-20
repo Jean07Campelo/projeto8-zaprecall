ed5b47b69ebd7fb16434eb1a46544935ca4ad4d2
Jean Campelo <jean93campelo@gmail.com>
2022-07-20 16:12:37 -0300

    perguntas base armazenadas em array de objetos

---
+++ src/Flashcards.js
@@ -0,0 +1,27 @@
+export default function Flashcards () {
+    return (
+        <>
+        <h1>Tela Contendo todos flashcards</h1>
+        </>
+    )
+}
+
+
+const flashcards = [
+  {
+    question: "O que é JSX?",
+    response: "Uma extensão de linguagem do JavaScript",
+  },
+  {
+    question: "O React é ...",
+    response: "uma biblioteca JavaScript para construção de interfaces",
+  },
+  {
+    question: "Componentes devem iniciar com ...",
+    response: "letra maiúscula",
+  },
+  {
+    question: "Podemos colocar ... dentro do JSX",
+    response: "expressões",
+  },
+];
