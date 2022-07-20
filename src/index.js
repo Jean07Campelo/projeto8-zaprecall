4cf81774dee3f83b9240e3a72512df9d21c6d318
Jean Campelo <jean93campelo@gmail.com>
2022-07-20 15:22:04 -0300

    Configurações iniciais, como CSS reset e style, carregamento do logo e linkamento do componente react com index

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+function App () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('.root'));
