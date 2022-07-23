43f471c2faba10bd3d32473a4f433763145cc2dc
Jean Campelo <jean93campelo@gmail.com>
2022-07-23 11:14:30 -0300

    progredindo para acessar objeto cards e alterar propriedade, resta atualizar cards com Spread Operator

--- src/Flashcards.js
+++ src/Flashcards.js
@@ -2,16 +2,17 @@ import React from "react";
 
 export default function Flashcards({ cards, setCards }) {
 
-  function turnedCard () {
+  function turnedCard(card) {
+        
+    const teste = (card.turned = (!card.turned))    
     
   }
 
   const renderCards = cards.map((card, index) => (
     <div className="card" key={index}>
       <h2>Pergunta {index + 1}</h2>
-
-      <ion-icon name="play-outline" onClick={turnedCard}></ion-icon>
-
+      {(card.turned ? 'true' : 'false')}
+      <ion-icon name="play-outline" onClick={()=>turnedCard(card)}></ion-icon>
     </div>
   ));
   console.log(cards);
