521b97a80ad82cfa8007ca51591e5eb81576a2d1
Jean Campelo <jean93campelo@gmail.com>
2022-07-24 17:33:09 -0300

    importação CSS e imagem

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 import Flashcards from "./Flashcards";
+import logo from "./assets/logo.png";
 
 export default function HomeScreen() {
-  const img = "../assets/logo.png";
   const description = "Logo ZapRecall, um raio amarelo com contorno vermelho";
 
-  const [screen, setScreen] = React.useState(false);
-  
+  const [screen, setScreen] = React.useState(true);
+
   return (
     <>
       {screen ? (
         <>
-          <img src={img} alt={description}></img>
+          <img src={logo} alt={description}></img>
           <h1>ZapRecall</h1>
           <button onClick={() => setScreen(!screen)}>Iniciar Recall!</button>
         </>
