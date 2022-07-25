9192c547002e98cdf7c2cd509783afecf4f8d5bb
Jean Campelo <jean93campelo@gmail.com>
2022-07-24 23:04:45 -0300

    lógica para mostrar três alternativas de zap

--- src/Flashcards.js
+++ src/Flashcards.js
@@ -2,12 +2,28 @@ import React from "react";
 
 export default function Flashcards({ cards }) {
   const [qtd, setQtd] = React.useState(0);
+  const [response, setResponse] = React.useState(false);
 
   const renderCard = cards.map((card, index) => (
     <>
       {card.open === "open" ? (
         <div className="card-open" key={index}>
           <h3>{card.question}</h3>
+          
+         
+          {response ? 
+          <>
+          <h2>hora de responder</h2>
+          <buttom>Não Lembrei</buttom>
+          <buttom>Quase não lembrei</buttom>
+          <buttom>Zap!</buttom>
+          </>
+          
+        :
+        <ion-icon name="reload-outline" onClick={()=>
+          setResponse(true)}></ion-icon>
+        }
+
         </div>
       ) : (
         <div className="card-closed" key={index}>
